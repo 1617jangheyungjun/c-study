4308146a86d46f76fd37c1b4ede57ee087ee553e
jangh <jangh4139@gmail.com>
2022-12-06 22:23:58 +0900

    사칙연산

---
+++ 사칙연산/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(0);
+Console.WriteLine(1 + 1);
+Console.WriteLine(2 - 1);
+Console.WriteLine(2 * 2);
+Console.WriteLine(6 / 2);
+Console.WriteLine(6 % 2);
