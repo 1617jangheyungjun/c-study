1f9d2b88f2ce20cbf111c9d6321cb860fc98f2e4
jangh <jangh4139@gmail.com>
2022-12-07 20:41:02 +0900

    변경

--- 사칙연산/Program.cs
+++ 사칙연산/Program.cs
@@ -5,3 +5,10 @@ Console.WriteLine(2 - 1);
 Console.WriteLine(2 * 2);
 Console.WriteLine(6 / 2);
 Console.WriteLine(6 % 2);
+
+Console.Write("Write");
+Console.Write("는");
+Console.Write("이어서 쓰는거");
+Console.WriteLine("");
+Console.WriteLine("WriteLine 은");
+Console.WriteLine("줄바뀜");
