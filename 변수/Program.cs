c2753fc0477668da7541b6485cb1de620354fc50
jangheyoungjun <jangh4139@gmail.com>
2022-12-09 10:04:06 +0900

    변수 업데이트

---
+++ 변수/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int f_int;
+int s_int;
+string massage1;
+string massage2;
+string massage3;
+f_int = 4;
+s_int = 5;
+massage1 = "첫 int형 변수는";
+massage2 = "두번째 int형 변수는";
+massage3 = "이다 변수끼리 더할 수 있다.";
+Console.WriteLine("{0} {1} {2} {3} {4} s_int + f_int = {5}", massage1, f_int, massage2, s_int, massage3, f_int+s_int);
