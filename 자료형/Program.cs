f10162e6ed275ad26a207314af86adeeaa5caad8
jangh <jangh4139@gmail.com>
2022-12-15 23:35:29 +0900

    조건문 추가

---
+++ 자료형/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int level;
+float hp;
+string PlayerName;
+
+level = 1;
+
+hp = 3.14f;
+
+PlayerName = "바나나";
+
+Console.WriteLine($"플레이어의 레벨 : {level}레벨");
+Console.WriteLine($"플레이어의 hp : {hp}");
+Console.WriteLine($"플레이어의 이름 : {PlayerName}");
---
+++ 조건문/Program.cs
@@ -0,0 +1,69 @@
+﻿int one = 10;
+int two = 10;
+//if문
+if(one == two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 같습니다.");
+    one += 1;
+}
+
+if(one == two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 같습니다.");
+    one += 1;
+}
+
+else if(one >= two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 one이 더 큽니다.");
+}
+
+if(one == two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 같습니다.");
+    one += 1;
+}
+
+else if(one <= two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 one이 더 작습니다.");
+}
+
+if(one == two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 같습니다.");
+    one += 1;
+}
+
+else if(one <= two)
+{
+    Console.WriteLine($"one({one})과 two({two})는 one이 더 큽니다.");
+}
+
+else
+{
+    Console.WriteLine($"one({one})과 two({two})는 one이 더 작습니다.");
+}
+
+//switch 문
+string day = "수";
+switch (day)
+{
+    case "일":
+        Console.WriteLine("일요일");
+        break;
+    case "월":
+        Console.WriteLine("월요일");
+        break;
+    case "화":
+        Console.WriteLine("화요일");
+        break;
+    case "수":
+        Console.WriteLine("수요일");
+        break;
+    default:
+        Console.WriteLine("아무 요일에도 해당하지 않습니다.");
+        break;
+    
+    
+}
