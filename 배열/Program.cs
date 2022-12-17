8d1bca78f3139fd6d34a6c759170865a7b54e064
jangh <jangh4139@gmail.com>
2022-12-18 00:09:37 +0900

    배열

---
+++ 배열/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace 배열;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] scores = new int[5];
+
+        scores[0] = 80;
+        scores[1] = 20;
+        scores[2] = 30;
+        scores[3] = 92;
+        scores[4] = 23;
+        for(int a = 0; a < scores.Length; a++)
+        {
+            Console.WriteLine(scores[a]);
+        }
+    }
+}
