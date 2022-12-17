dba824004520a70cc48d0e2055b2a64a63d74c75
jangh <jangh4139@gmail.com>
2022-12-17 23:50:42 +0900

    반복문

---
+++ 반복문/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace 반복문;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int playerExp = 10;
+        //while 문
+        while(playerExp > 0)
+        {
+            Console.WriteLine($"Player : {playerExp}");
+            playerExp--;
+        }
+        for (int playerGold = 0; playerGold < 5; playerGold++)
+        {
+            Console.WriteLine($"playerGold : {playerGold}");
+        }
+    }
+}
