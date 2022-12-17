5162c86a05f88c67d17f08071509a9b73a481f3d
jangh <jangh4139@gmail.com>
2022-12-18 00:54:44 +0900

    메서드

---
+++ 메서드/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace 메서드;
+class Program
+{
+    static void ShowHello()
+    {
+        Console.WriteLine("Hello World");
+    }
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+    static void Main(string[] args)
+    {
+        ShowHello();
+        Console.WriteLine(add(12, 23));
+    }
+}
