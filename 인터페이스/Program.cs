e643f6cedae7433c7892ef8edc98c4059b09a41b
jangh <jangh4139@gmail.com>
2022-12-30 22:48:50 +0900

    인터페이스까지

---
+++ 인터페이스/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace 인터페이스;
+interface IAnimal
+{
+    void Eat();
+}
+class Person : IAnimal
+{
+    public void Eat()
+    {
+        Console.WriteLine("밥을 먹을 수 있습니다.");
+    }
+}
+class Lion : IAnimal
+{
+    public void Eat()
+    {
+        Console.WriteLine("고기를 먹습니다");
+    }
+}
+class Camel
+{
+    public void Eat()
+    {
+        Console.WriteLine("풀을 먹습니다.");
+    }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        Person myPerson = new Person();
+        Lion myLion = new Lion();
+        Camel  myCamel = new Camel();
+        myPerson.Eat();
+        myLion.Eat();
+        myCamel.Eat();
+    }
+}
---
+++ 클래스/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace 클래스;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog puppy;
+        Dog dog;
+        puppy = new Dog();
+        puppy.Bark();
+        dog = new Dog();
+        dog.Waite();
+    }
+}
+class Dog
+{
+    public void Bark()
+    {
+        Console.WriteLine("월월");
+    }
+    public void Waite()
+    {
+        Console.WriteLine("으르르");
+    }
+}
