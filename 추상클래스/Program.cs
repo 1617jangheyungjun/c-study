41b6e43aeab1ec33e166a77a26957bd511f02017
jangh <jangh4139@gmail.com>
2022-12-31 17:19:56 +0900

    추상클래스까지

---
+++ 추상클래스/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace 추상클래스;
+class Program
+{
+    public abstract class abstractAnimal
+    {
+        public abstract void Sound();
+    }
+    public class Dog : abstractAnimal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("멍멍");
+        }
+    }
+    public class Cat : abstractAnimal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("야옹야옹");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Dog puppy = new Dog();
+        Cat happy_cat = new Cat();
+
+        puppy.Sound();
+        happy_cat.Sound();
+    }
+}
